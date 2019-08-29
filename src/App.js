f3e3c2b98240d50aeddf412a30ded4bf6c761a22
Anton Starostin <antons.starostins@gmail.com>
2019-08-29 15:56:35 +0300

    change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome this is Anton message!</h1>
+          <h1 className="App-title">
+            Welcome this is Anton message22222222222!
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
